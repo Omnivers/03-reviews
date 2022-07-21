b99a47c44abd7406d6ba03867f4ec4554f15f933
Omnivers <Twersimor.stock@outlook.com>
2022-07-21 23:42:40 +0200

    Updates

---
+++ precache-manifest.c1e3cb6a009c277b1020f364d14bee57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce830a7533699a3b8509e8bd213caadc",
+    "url": "/03-reviews/index.html"
+  },
+  {
+    "revision": "d0081d71b88835fb8915",
+    "url": "/03-reviews/static/css/main.e64ab053.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/03-reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/03-reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0081d71b88835fb8915",
+    "url": "/03-reviews/static/js/main.ed6b06a3.chunk.js"
+  },
+  {
+    "revision": "65b4b856b7fe821dc99f",
+    "url": "/03-reviews/static/js/runtime-main.bacaca2c.js"
+  }
+]);
